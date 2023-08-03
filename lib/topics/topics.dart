f37656b1a1531cbe3b75606858a00c3635a6c381
AndreAleman <davidandrealeman@gmail.com>
2023-08-02 22:26:14 -0400

    Added bottom nav

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,10 +1,16 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:quizappbackup/shared/shared.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class TopicsScreen extends StatelessWidget {
   const TopicsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: const BottomNavBar(),
+    );
   }
 }
